1f0b0b8e3b3acecaa585f1b23ecf19d08d7b7a24
jimmymk23 <jimkeseling@gmail.com>
2021-04-29 05:20:33 -0400

    Less output spacing

--- bin/nextjs-template-bare.js
+++ bin/nextjs-template-bare.js
@@ -9,7 +9,7 @@ spawnSync('git', ['clone', url, `${process.cwd()}/${appName}`]);
 spawnSync('npm', ['install', '--prefix', `${process.cwd()}/${appName}`]);
 
 console.log(
-	'\n\n\n\nYour bare-bones Next.js starter template has been successfully installed!\n\n'
+	'\n\nYour bare-bones Next.js starter template has been successfully installed!\\n'
 );
 console.log(`$ cd ${appName}`);
 console.log('$ npm start\n\n');
